74430b8bb1c2ad1de6cbefeaba36ce28835ad9d1
Sage Weil <sage@redhat.com>
2014-12-09 16:36:15 -0800

    osd: scrub: disqualify shards with digests that disagree with oi digest
    The auth may or may not know that the digest is truly bad.  Note that we
    can only relate scrub digests to those in oi if the scrub used a seed of
    -1; older OSDs use 0.
    
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/osd/PG.cc
+++ src/osd/PG.cc
@@ -3989,6 +3989,7 @@ void PG::scrub_compare_maps()
 
     get_pgbackend()->be_compare_scrubmaps(
       maps,
+      scrubber.seed == 0xffffffff,  // can we relate scrub digests to oi digests?
       scrubber.missing,
       scrubber.inconsistent,
       authoritative,
